b7877bae8c457415b9e0bf2a04bcddeb04caca41
Jouni Roivas <jroivas@iki.fi>
2014-11-22 16:29:03 +0200

    Simple per line code generation

--- compiler/assemble.py
+++ compiler/assemble.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import opcodes
 
 
 def read(fname):
@@ -17,18 +18,27 @@ class ParseError(Exception):
 class Parser:
     def __init__(self, data):
         self.data = data
-        self.code = []
+        self.code = ''
+        self.output = {}
         self.debug = True
         self.labels = {}
-        self.pos = 0
         self.line = 0
+        self.regmap = {}
+
+    def __str__(self):
+        r = ''
+        for l in self.output:
+            r += '%s: %s\n' % (l, self.output[l])
+        return r
+        #return self.code
 
     def parse_label(self, data):
         if not data.isalnum():
             return False
         if self.debug:
             print ("LABEL: %s" % (data))
-        self.labels[data] = self.pos
+        #self.labels[data] = len(self.code)
+        self.labels[data] = self.line
         return True
 
     def raw_number(self, num):
@@ -93,31 +103,98 @@ class Parser:
 
         return (num_bytes, num)
 
+    def is_float(self, val):
+        for c in val:
+            if not (c.isdigit() or c == '.'):
+                return False
+        return True
+
+    def parse_reg(self, regdata):
+        if regdata != 'PC' and regdata.upper()[0] != 'R':
+            raise ParseError("Invalid register: %s @%s" % (regdata, self.line))
+
+        if regdata == 'PC':
+            return -1
+
+        reg = int(regdata[1:])
+        if reg == 0:
+            raise ParseError("Invalid register: %s @%s" % (regdata, self.line))
+        reg -= 1
+
+        return reg
+
+    def output_num(self, val, full=True):
+        res = self.fill_to_bytes(self.raw_number(val))
+        if full:
+            return res
+        return res[1]
+
     def parse_store(self, opts):
         data = [x.strip() for x in opts.split(',')]
-        print data
         if len(data) != 2:
             raise ParseError("Invalid number argument for STORE: %s (%s) @%s" % (len(data), opts, self.line))
 
-        if data[0] != 'PC' and data[0].upper()[0] != 'R':
-            raise ParseError("Invalid register: %s @%s" % (data[0], self.line))
+        reg = self.parse_reg(data[0])
 
         value = data[1]
         if value.isdigit():
             # Int
             val = int(value)
-            (_, val) = self.fill_to_bytes(self.raw_number(val))
-            print ("VV", val, value)
-        #if value[0] == '"' and value[-1] == '"':
-        #    # String
-        #    pass
+            (cnt, val) = self.output_num(val)
+            if cnt == 1:
+                self.code += chr(opcodes.STORE_INT8)
+            elif cnt == 2:
+                self.code += chr(opcodes.STORE_INT16)
+            elif cnt == 4:
+                self.code += chr(opcodes.STORE_INT32)
+            elif cnt == 8:
+                self.code += chr(opcodes.STORE_INT64)
+
+            self.regmap[reg] = 'int'
+            self.code += self.output_num(reg, False)
+            self.code += val
+        elif value.isfloat():
+            # FIXME
+            # Float
+            self.regmap[reg] = 'float'
+            pass
+        else:
+            # String
+            self.code += chr(opcodes.STORE_STR)
+            self.code += self.output_num(reg, False)
+            self.regmap[reg] = 'str'
+            self.code += value + '\x00'
+
+    def parse_print(self, opts):
+        opts = opts.strip()
+        if opts[0] == 'R':
+            reg = self.parse_reg(opts)
+            if not reg in self.regmap:
+                raise ParseError("Using unused register for PRINT: %s @%s" % (opts, self.line))
+            if self.regmap[reg] == 'int':
+                self.code += chr(opcodes.PRINT_INT)
+            elif self.regmap[reg] == 'str':
+                self.code += chr(opcodes.PRINT_STR)
+            self.code += self.output_num(reg, False)
+            print ("print %s %s" % (self.regmap[reg], reg))
+        elif opts in self.labels:
+            self.code += 'FIXME'
+            self.code += chr(opcodes.PRINT_STR)
+            print ("print %s" % (opts))
 
     def parse_command(self, cmd, opts):
         cmd = cmd.upper()
         if cmd == 'STORE':
             self.parse_store(opts)
+        elif cmd == 'PRINT':
+            self.parse_print(opts)
         else:
             print cmd, opts
+
+    def pre_parse_line(self, line):
+        datas = line.split(':')
+        if len(datas) >= 2 and self.parse_label(datas[0]):
+            return
 
     def parse_line(self, line):
         datas = line.split(':')
@@ -132,12 +209,25 @@ class Parser:
 
         self.parse_command(cmds[0], ' '.join(cmds[1:]))
 
-    def parse(self):
+    def pre_parse(self):
         for self.line, line in enumerate(self.data):
             line = line.strip()
             if not line or line[0] == ';':
                 continue
+            self.pre_parse_line(line)
+
+    def parse(self):
+        self.pre_parse()
+        for self.line, line in enumerate(self.data):
+            self.code = ''
+
+            line = line.strip()
+            if not line or line[0] == ';':
+                continue
+
             self.parse_line(line)
+
+            self.output[self.line] = self.code
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
@@ -148,3 +238,4 @@ if __name__ == '__main__':
     p = Parser(data)
     p.parse()
 
+    print "res: %s" % (p)
