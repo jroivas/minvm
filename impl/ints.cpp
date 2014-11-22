d3691d00fd4f971466f218f30d73a911d2d1432c
Jouni Roivas <jroivas@iki.fi>
2014-11-22 10:16:30 +0200

    Refactor, split ints and strings

--- impl/ints.cpp
+++ impl/ints.cpp
@@ -1,23 +1,21 @@
-#include "asm1.hh"
+#include "ints.hh"
 #include "opcodes.hh"
 #include <iostream>
 
 using core::VM;
 using core::Opcode;
+using impl::Ints;
 
-ASM1::ASM1(VM *vm)
+Ints::Ints(VM *vm)
 {
-    vm->opcode((uint32_t)Opcode::STORE_INT8, ASM1::store_int8);
-    vm->opcode((uint32_t)Opcode::STORE_INT32, ASM1::store_int32);
-    vm->opcode((uint32_t)Opcode::STORE_INT64, ASM1::store_int64);
-    vm->opcode((uint32_t)Opcode::ADD_INT, ASM1::add_int);
-    vm->opcode((uint32_t)Opcode::PRINT_INT, ASM1::print_int);
-
-    vm->opcode((uint32_t)Opcode::STORE_STR, ASM1::store_str);
-    vm->opcode((uint32_t)Opcode::PRINT_STR, ASM1::print_str);
+    vm->opcode((uint32_t)Opcode::STORE_INT8, Ints::store_int8);
+    vm->opcode((uint32_t)Opcode::STORE_INT32, Ints::store_int32);
+    vm->opcode((uint32_t)Opcode::STORE_INT64, Ints::store_int64);
+    vm->opcode((uint32_t)Opcode::ADD_INT, Ints::add_int);
+    vm->opcode((uint32_t)Opcode::PRINT_INT, Ints::print_int);
 }
 
-bool ASM1::store_int8(VM *vm)
+bool Ints::store_int8(VM *vm)
 {
     if (vm->debug()) std::cerr << "STORE_INT8\n";
     uint8_t reg = vm->fetch8();
@@ -27,7 +25,7 @@ bool ASM1::store_int8(VM *vm)
     return true;
 }
 
-bool ASM1::store_int32(VM *vm)
+bool Ints::store_int32(VM *vm)
 {
     if (vm->debug()) std::cerr << "STORE_INT32\n";
     uint8_t reg = vm->fetch8();
@@ -41,7 +39,7 @@ bool ASM1::store_int32(VM *vm)
     return true;
 }
 
-bool ASM1::store_int64(VM *vm)
+bool Ints::store_int64(VM *vm)
 {
     if (vm->debug()) std::cerr << "STORE_INT64\n";
     uint8_t reg = vm->fetch8();
@@ -55,7 +53,7 @@ bool ASM1::store_int64(VM *vm)
     return true;
 }
 
-bool ASM1::add_int(VM *vm)
+bool Ints::add_int(VM *vm)
 {
     if (vm->debug()) std::cerr << "ADD_INT\n";
     uint8_t reg1 = vm->fetch8();
@@ -70,34 +68,10 @@ bool ASM1::add_int(VM *vm)
     return true;
 }
 
-bool ASM1::print_int(VM *vm)
+bool Ints::print_int(VM *vm)
 {
     if (vm->debug()) std::cerr << "PRINT_INT\n";
     uint8_t reg = vm->fetch8();
     std::cout << vm->regs().load_int(reg);
-    return true;
-}
-
-bool ASM1::store_str(core::VM *vm)
-{
-    if (vm->debug()) std::cerr << "STORE_STR\n";
-    std::string res = "";
-    uint8_t reg = vm->fetch8();
-
-    uint8_t val = vm->fetch8();
-    while (val != 0) {
-        res.push_back(val);
-        val = vm->fetch8();
-    }
-    vm->regs().store_string(reg, res);
-
-    return true;
-}
-
-bool ASM1::print_str(core::VM *vm)
-{
-    if (vm->debug()) std::cerr << "PRINT_STR\n";
-    std::cout << vm->regs().load_string(vm->fetch8());
-
     return true;
 }
