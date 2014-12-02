23c5238e496d99bc2ad43111fa2b029a1347d85f
Jouni Roivas <jroivas@iki.fi>
2014-12-02 21:55:31 +0200

    Argument parsing

--- main.cpp
+++ main.cpp
@@ -14,20 +14,66 @@
 
 using namespace core;
 
+#include <map>
+#include <string>
+
+void usage(std::string app)
+{
+    std::cout << "Usage: " << app << " application\n";
+    std::cout << "  -h|--help      This help\n";
+    std::cout << "  -d|--debug     Set debug\n";
+}
+
+std::map<std::string, std::string> parseArgs(int argc, char **argv)
+{
+    bool got_app = false;
+    std::map<std::string, std::string> res;
+    for (int i = 1; i < argc; ++i) {
+        std::string val = argv[i];
+        if (val == "-d" ||
+            val == "--debug") {
+            res["debug"] = "true";
+        } else if (val == "-h" ||
+            val == "--help") {
+            usage(argv[0]);
+            exit(0);
+        } else if (val[0] != '-') {
+            if (got_app) {
+                std::cout << "\nERROR: Application already defined,"
+                    << " invalid argument: " << val << "\n\n";
+                usage(argv[0]);
+                exit(1);
+            }
+            got_app = true;
+            res["fname"] = val;
+        } else {
+            std::cout << "\nERROR: Invalid arugment: " << val << "\n\n";
+            usage(argv[0]);
+            exit(1);
+        }
+    }
+    return res;
+}
+
 int main(int argc, char **argv)
 {
-    if (argc <= 1) {
-        std::cout << "Usage: " << argv[0] << " application\n";
+    std::map<std::string, std::string> args = parseArgs(argc, argv);
+    auto fname = args.find("fname");
+    if (fname == args.end()) {
+        std::cout << "\nERROR: Missing application!\n\n";
+        usage(argv[0]);
         return 1;
     }
 
-    std::ifstream input(argv[1], std::ios::in | std::ios::binary);
+    std::ifstream input(fname->second, std::ios::in | std::ios::binary);
     std::string code(
         (std::istreambuf_iterator<char>(input)),
         std::istreambuf_iterator<char>());
 
     VM vm((uint8_t*)code.data(), code.length());
-    //vm.set_debug();
+    auto debug = args.find("debug");
+    if (debug != args.end())
+        vm.set_debug();
 
     impl::NopStop nopstop(&vm);
     impl::Ints ints(&vm);
